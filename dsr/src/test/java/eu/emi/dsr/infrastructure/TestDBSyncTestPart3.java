9c7e229eb0cc800db4ae4fb41629b776f34ada07
Gabor Szigeti <szigeti@niif.hu>
2011-08-22 15:46:53 +0200

    file name modification and add testcases

---
+++ dsr/src/test/java/eu/emi/dsr/infrastructure/TestDBSyncTestPart3.java
@@ -0,0 +1,183 @@
+/**
+ * 
+ */
+package eu.emi.dsr.infrastructure;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.junit.Test;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.ClientResponse.Status;
+import com.sun.jersey.api.client.WebResource;
+
+import eu.emi.dsr.client.DSRClient;
+import eu.emi.dsr.core.ServiceBasicAttributeNames;
+import eu.emi.dsr.util.DateUtil;
+import eu.emi.dsr.util.ServiceUtil;
+
+/**
+ * <li>cleanup</li> <li>start child server and mongodb instance</li> <li>start
+ * parent server with same mongodb instance (perhaps a different collection
+ * name)</li> <li>register with the client-dsr 4. check if the parent dsr have
+ * received the notification and updated its database</li> <li>cleanup</li>
+ * 
+ * Follow the similar steps for update, delete and expire registrations.
+ * 
+ * Pre-condition: stopped the parent server only child should be running in a separate jvm
+ * 
+ * @author g.szigeti
+ */
+public class TestDBSyncTestPart3 {
+
+	@Test
+	public void testDelayedRemove() throws JSONException, IOException, InterruptedException{
+		//remove one exist entry
+		getChildClient("/serviceadmin?Service_Endpoint_URL=http://3").delete();
+		try {
+			getChildClient("/serviceadmin")
+					.queryParam(
+							ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+									.getAttributeName(),
+							"http://3").get(JSONObject.class);
+		} catch (UniformInterfaceException e) {
+			assertTrue(e.getResponse().getStatus() == Status.NO_CONTENT
+					.getStatusCode());
+		}
+
+		
+		//remove one none exist entry
+		try {
+			getChildClient("/serviceadmin?Service_Endpoint_URL=http://non_exist").delete();
+			getChildClient("/serviceadmin")
+					.queryParam(
+							ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+									.getAttributeName(),
+							"http://non_exist").get(JSONObject.class);
+		} catch (UniformInterfaceException e) {
+			assertTrue(e.getResponse().getStatus() == Status.UNAUTHORIZED
+					.getStatusCode());
+		}
+	}
+
+	@Test
+	public void testDelayedUpdateRemove() throws JSONException, IOException, InterruptedException{
+		// one update to the child server
+		JSONObject jo = new JSONObject(
+				ServiceUtil
+						.convertFileToString("src/test/resources/serviceinfo2.json"));
+		jo = DateUtil.setExpiryTime(jo, 12);
+
+		// Updating the entry
+		System.out.println("updateing: " + jo);
+		jo.put(ServiceBasicAttributeNames.SERVICE_ENDPOINT_HEALTH_STATEINFO
+				.getAttributeName(), "health-state-info-changed");
+
+		ClientResponse res = getChildClient("/serviceadmin?Service_Endpoint_URL").accept(
+				MediaType.APPLICATION_JSON_TYPE).put(ClientResponse.class, jo);
+		assertTrue(res.getStatus() == Status.OK.getStatusCode());
+		
+		Thread.sleep(1000);
+
+		//remove one exist entry
+		getChildClient("/serviceadmin?Service_Endpoint_URL=http://2").delete();
+		try {
+			getChildClient("/serviceadmin")
+					.queryParam(
+							ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+									.getAttributeName(),
+							"http://2").get(JSONObject.class);
+		} catch (UniformInterfaceException e) {
+			assertTrue(e.getResponse().getStatus() == Status.NO_CONTENT
+					.getStatusCode());
+		}
+	}
+	
+	@Test
+	public void testDelayedUpdate() throws JSONException, IOException, InterruptedException{
+		// one update to the child server
+		JSONObject jo = new JSONObject(
+				ServiceUtil
+						.convertFileToString("src/test/resources/serviceinfo.json"));
+		jo = DateUtil.setExpiryTime(jo, 12);
+
+		// Updating the entry
+		System.out.println("updateing: " + jo);
+		jo.put(ServiceBasicAttributeNames.SERVICE_ENDPOINT_HEALTH_STATE
+				.getAttributeName(), "bussy");
+
+		ClientResponse res = getChildClient("/serviceadmin?Service_Endpoint_URL").accept(
+				MediaType.APPLICATION_JSON_TYPE).put(ClientResponse.class, jo);
+		assertTrue(res.getStatus() == Status.OK.getStatusCode());
+		
+		Thread.sleep(1000);
+
+		//Update check
+		JSONObject childJO = getChildClient(
+				"/serviceadmin?Service_Endpoint_URL=http://1").accept(
+				MediaType.APPLICATION_JSON_TYPE).get(JSONObject.class);
+
+		assertEquals(jo.get(ServiceBasicAttributeNames.SERVICE_ENDPOINT_HEALTH_STATEINFO
+				.getAttributeName()),
+				childJO.get(ServiceBasicAttributeNames.SERVICE_ENDPOINT_HEALTH_STATEINFO
+						.getAttributeName()));
+	}
+	
+	@Test
+	public void testDelayedRegistrationRemove() throws JSONException, IOException, InterruptedException{
+		// one registration to the child server
+		JSONObject jo = new JSONObject(
+				ServiceUtil
+						.convertFileToString("src/test/resources/serviceinfo4.json"));
+		jo = DateUtil.setExpiryTime(jo, 12);
+		System.out.println("registering: " + jo);
+		ClientResponse res = getChildClient("/serviceadmin").accept(
+				MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class, jo);
+		assertTrue(res.getStatus() == Status.OK.getStatusCode());
+
+		Thread.sleep(2000);
+
+		//Registration check
+		JSONObject childJO4 = getChildClient(
+				"/serviceadmin?Service_Endpoint_URL=http://4").accept(
+				MediaType.APPLICATION_JSON_TYPE).get(JSONObject.class);
+
+		assertEquals(jo.get(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+				.getAttributeName()),
+				childJO4.get(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+						.getAttributeName()));
+		
+		Thread.sleep(2000);
+		
+		//remove one exist entry
+		getChildClient("/serviceadmin?Service_Endpoint_URL=http://4").delete();
+		try {
+			getChildClient("/serviceadmin")
+					.queryParam(
+							ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+									.getAttributeName(),
+							"http://4").get(JSONObject.class);
+		} catch (UniformInterfaceException e) {
+			assertTrue(e.getResponse().getStatus() == Status.NO_CONTENT
+					.getStatusCode());
+		}
+
+	}
+	
+	protected WebResource getChildClient(String path) {
+		DSRClient c = new DSRClient("http://localhost:9000" + path);
+		return c.getClientResource();
+	}
+
+	protected WebResource getParentClient(String path) {
+		DSRClient c = new DSRClient("http://localhost:9001" + path);
+		return c.getClientResource();
+	}
+
+}
