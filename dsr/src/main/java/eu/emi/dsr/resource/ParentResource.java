e684ed28f31e25d0af869424ae7163c9fe857a05
Gabor Szigeti <szigeti@niif.hu>
2011-11-02 13:56:17 +0100

    Add parent resource

---
+++ dsr/src/main/java/eu/emi/dsr/resource/ParentResource.java
@@ -0,0 +1,24 @@
+package eu.emi.dsr.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import eu.emi.dsr.DSRServer;
+import eu.emi.dsr.core.ServerConstants;
+
+/**
+ * Checking to the emiregistry and get list of childs
+ * @author g.szigeti
+ *
+ */
+@Path("/parent")
+public class ParentResource {
+
+	@GET
+	public Response childDSRs(){
+		String parent = DSRServer.getProperty(ServerConstants.REGISTRY_PARENT_URL);
+		return Response.ok().entity(parent).build();
+	}
+
+}
