979b8fe082faf15197da6332f849504579637ad1
Shiraz Memon <ahmed.shiraz.memon@gmail.com>
2011-11-28 15:29:27 +0100

    - adding ACL the default filter if xacml is not set - properly handling owner -- if owner tries to register without the owner attribute then it will use admin's dn as owner

--- dsr/src/main/java/eu/emi/dsr/resource/ServiceAdminResource.java
+++ dsr/src/main/java/eu/emi/dsr/resource/ServiceAdminResource.java
@@ -59,7 +59,7 @@ public class ServiceAdminResource {
 	 */
 	public ServiceAdminResource() {
 		// serviceAdmin = ServiceManagerFactory.getServiceAdminManager();
-			serviceAdmin = new ServiceAdminManager();
+		serviceAdmin = new ServiceAdminManager();
 	}
 
 	protected String getUserPrincipalName() {
@@ -76,7 +76,7 @@ public class ServiceAdminResource {
 	}
 
 	@GET
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	public JSONObject getServicebyUrl(@Context UriInfo infos)
 			throws WebApplicationException, JSONException {
 		logger.debug("getting service by url");
@@ -84,16 +84,18 @@ public class ServiceAdminResource {
 		try {
 			result = serviceAdmin
 					.findServiceByUrl(extractServiceUrlFromUri(infos));
-		} catch (Exception e){
+		} catch (Exception e) {
 			JSONObject jErr = new JSONObject();
 			jErr.put("error", e.getCause());
-			throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
-		} 
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+		}
 
 		return result;
 	}
 
-	private String extractServiceUrlFromUri(UriInfo infos) throws IllegalArgumentException{
+	private String extractServiceUrlFromUri(UriInfo infos)
+			throws IllegalArgumentException {
 		MultivaluedMap<String, String> mm = infos.getQueryParameters();
 		String attrName = ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
 				.getAttributeName();
@@ -109,95 +111,115 @@ public class ServiceAdminResource {
 
 	/**
 	 * adding only one entry
-	 * @throws JSONException 
+	 * 
+	 * @throws JSONException
 	 * */
-	
-	
+
 	public Response registerService(JSONObject serviceInfo)
-			throws WebApplicationException, JSONException{
+			throws WebApplicationException, JSONException {
 		Integer length = serviceInfo.length();
 		if (length <= 0 || length > 100) {
 			throw new WebApplicationException(Status.FORBIDDEN);
 		}
 
-		
 		try {
 			Client c = (Client) req.getAttribute("client");
-			serviceInfo.put(ServiceBasicAttributeNames.SERVICE_OWNER
-					.getAttributeName(), c.getDistinguishedName());
+			serviceInfo
+					.put(ServiceBasicAttributeNames.SERVICE_OWNER
+							.getAttributeName(), c.getDistinguishedName());
 			JSONObject res = serviceAdmin.addService(serviceInfo);
 			return Response.ok(res).build();
 		} catch (ExistingResourceException e) {
 			return Response.status(Status.CONFLICT).entity(serviceInfo).build();
-		} catch (Exception e){
+		} catch (Exception e) {
 			JSONObject jErr = new JSONObject();
 			jErr.put("error", e.getCause());
-			throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
-		} 
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+		}
 	}
-	
+
 	/**
-	 * adding an array of json documents, where each item in the array is service endpoint information
-	 * @throws InterruptedException 
-	 * TODO: polymorphic registrations: Supporting JSONObject as well as Array
-	 * @throws JSONException 
+	 * adding an array of json documents, where each item in the array is
+	 * service endpoint information
+	 * 
+	 * @throws InterruptedException
+	 *             TODO: polymorphic registrations: Supporting JSONObject as
+	 *             well as Array
+	 * @throws JSONException
 	 * */
 	@POST
-	@Consumes({MediaType.APPLICATION_JSON})
-	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
 	public Response registerServices(JSONArray serviceInfos)
-			throws WebApplicationException, InterruptedException, JSONException{
-		Long max = Long.valueOf(DSRServer.getProperty(ServerConstants.REGISTRY_MAX_REGISTRATIONS, "100"));
+			throws WebApplicationException, InterruptedException, JSONException {
+		Long max = Long.valueOf(DSRServer.getProperty(
+				ServerConstants.REGISTRY_MAX_REGISTRATIONS, "100"));
 		if (serviceInfos.length() > max) {
-			return Response.status(Status.FORBIDDEN).entity(new String("Number of entries/json objects in the array must not exceed: "+max)).build();
+			return Response
+					.status(Status.FORBIDDEN)
+					.entity(new String(
+							"Number of entries/json objects in the array must not exceed: "
+									+ max)).build();
 		}
 		JSONObject serviceInfo = null;
 		JSONArray arr = new JSONArray();
 		JSONArray errorArray = new JSONArray();
-		for ( int i=0; i< serviceInfos.length(); i++ ) {
+		for (int i = 0; i < serviceInfos.length(); i++) {
 			try {
 				serviceInfo = serviceInfos.getJSONObject(i);
 				Integer length = serviceInfo.length();
 				if (length <= 0 || length > 100) {
 					throw new WebApplicationException(Status.FORBIDDEN);
 				}
-		
+
 				Client c = (Client) req.getAttribute("client");
 				JSONObject res = null;
-				//let the admin add entries from others
-				if (c.getRole().getName().equalsIgnoreCase("admin") && serviceInfo.has(ServiceBasicAttributeNames.SERVICE_OWNER
-						.getAttributeName())) {
-					//add directly to the mongodb
+				// let the admin add entries from others
+				if (c.getRole().getName().equalsIgnoreCase("admin")) {
+					if (!serviceInfo
+							.has(ServiceBasicAttributeNames.SERVICE_OWNER
+									.getAttributeName())) {
+
+						serviceInfo.put(
+								ServiceBasicAttributeNames.SERVICE_OWNER
+										.getAttributeName(), c
+										.getDistinguishedName());
+
+					}
 					res = serviceAdmin.addService(serviceInfo);
+
 				} else {
-					//add if the owner is missing 
+					// add if the owner is missing
 					serviceInfo.put(ServiceBasicAttributeNames.SERVICE_OWNER
 							.getAttributeName(), c.getDistinguishedName());
 					res = serviceAdmin.addService(serviceInfo);
 				}
 				arr.put(res);
 				continue;
-				//return Response.ok(res).build();
+				// return Response.ok(res).build();
 			} catch (ExistingResourceException e) {
 				errorArray.put(serviceInfo);
-			} catch (Exception e){
+			} catch (Exception e) {
 				JSONObject jErr = new JSONObject();
 				jErr.put("error", e.getCause());
-				throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+				throw new WebApplicationException(Response
+						.status(Status.INTERNAL_SERVER_ERROR).entity(jErr)
+						.build());
 			}
-			
+
 		}
 		EventDispatcher.notifyRecievers(new Event(EventTypes.SERVICE_ADD, arr));
-		if (errorArray.length()>0){
+		if (errorArray.length() > 0) {
 			return Response.status(Status.CONFLICT).entity(errorArray).build();
 		}
 		return Response.ok(arr).build();
 	}
 
-
 	/**
 	 * updating only one entry
-	 * @throws JSONException 
+	 * 
+	 * @throws JSONException
 	 * */
 
 	public Response updateService(JSONObject serviceInfo)
@@ -206,14 +228,13 @@ public class ServiceAdminResource {
 			Client c = (Client) req.getAttribute("client");
 			String owner = c.getDistinguishedName();
 			String url = serviceInfo
-			.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
-					.getAttributeName());
+					.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+							.getAttributeName());
 			serviceInfo
 					.put(ServiceBasicAttributeNames.SERVICE_OWNER
 							.getAttributeName(), c.getDistinguishedName());
 			if (logger.isDebugEnabled()) {
-				logger.debug("updating service by url: "
-						+ url + ", Owned by: "
+				logger.debug("updating service by url: " + url + ", Owned by: "
 						+ owner);
 			}
 
@@ -226,56 +247,64 @@ public class ServiceAdminResource {
 													.getAttributeName()))) {
 				JSONObject res;
 				try {
-					res = serviceAdmin.updateService(serviceInfo);					
+					res = serviceAdmin.updateService(serviceInfo);
 				} catch (UnknownServiceException e) {
 					return Response.status(Status.NOT_FOUND).build();
 				}
 				return Response.ok(res).build();
 			} else {
 				if (logger.isDebugEnabled()) {
-					logger.debug("Service with url: "+serviceInfo.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL.getAttributeName())+" does not exist.");
+					logger.debug("Service with url: "
+							+ serviceInfo
+									.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+											.getAttributeName())
+							+ " does not exist.");
 				}
-				return Response.status(Status.UNAUTHORIZED).entity("Access denied for DN - "+owner+" to update service with the URL - "+url).build();
+				return Response
+						.status(Status.UNAUTHORIZED)
+						.entity("Access denied for DN - " + owner
+								+ " to update service with the URL - " + url)
+						.build();
 			}
 
-		} catch (Exception e){
+		} catch (Exception e) {
 			JSONObject jErr = new JSONObject();
 			jErr.put("error", e.getCause());
-			throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
-		} 
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+		}
 	}
 
 	/**
 	 * updating array of entries
-	 * @throws JSONException 
+	 * 
+	 * @throws JSONException
 	 * */
 
 	@PUT
-	@Consumes({MediaType.APPLICATION_JSON})
-	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
 	public Response updateServices(JSONArray serviceInfos)
 			throws WebApplicationException, JSONException {
 		try {
 			JSONArray arr = new JSONArray();
 			JSONArray errorArray = new JSONArray();
-			for ( int i=0; i< serviceInfos.length(); i++ ) {
+			for (int i = 0; i < serviceInfos.length(); i++) {
 				JSONObject serviceInfo = serviceInfos.getJSONObject(i);
 				Client c = (Client) req.getAttribute("client");
 				String owner = c.getDistinguishedName();
 				String url = serviceInfo
 						.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
 								.getAttributeName());
-				serviceInfo
-					.put(ServiceBasicAttributeNames.SERVICE_OWNER
-							.getAttributeName(), c.getDistinguishedName());
+				serviceInfo.put(ServiceBasicAttributeNames.SERVICE_OWNER
+						.getAttributeName(), c.getDistinguishedName());
 				if (logger.isDebugEnabled()) {
-					logger.debug("updating service by url: "
-							+ url + ", Owned by: "
-							+ owner);
+					logger.debug("updating service by url: " + url
+							+ ", Owned by: " + owner);
 				}
 
-				if(c.getRole().getName().equalsIgnoreCase("admin")){
-					//let the admin update any service
+				if (c.getRole().getName().equalsIgnoreCase("admin")) {
+					// let the admin update any service
 					JSONObject res;
 					try {
 						res = serviceAdmin.updateService(serviceInfo);
@@ -286,10 +315,10 @@ public class ServiceAdminResource {
 						errorArray.put(serviceInfo);
 					}
 					continue;
-					
+
 				} else if (owner != null
 						&& serviceAdmin
-							.checkOwner(
+								.checkOwner(
 										owner,
 										serviceInfo
 												.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
@@ -304,35 +333,47 @@ public class ServiceAdminResource {
 						errorArray.put(serviceInfo);
 					}
 					continue;
-					//return Response.ok(res).build();
-				}  else {
+					// return Response.ok(res).build();
+				} else {
 					if (logger.isDebugEnabled()) {
-						logger.debug("Service with url: "+serviceInfo.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL.getAttributeName())+" does not exist.");
+						logger.debug("Service with url: "
+								+ serviceInfo
+										.getString(ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+												.getAttributeName())
+								+ " does not exist.");
 					}
-					return Response.status(Status.UNAUTHORIZED).entity("Access denied for DN - "+owner+" to update service with the URL - "+url).build();
+					return Response
+							.status(Status.UNAUTHORIZED)
+							.entity("Access denied for DN - " + owner
+									+ " to update service with the URL - "
+									+ url).build();
 				}
 			}
-			EventDispatcher.notifyRecievers(new Event(EventTypes.SERVICE_UPDATE, arr));
-			if (errorArray.length()>0){
-				return Response.status(Status.CONFLICT).entity(errorArray).build();
+			EventDispatcher.notifyRecievers(new Event(
+					EventTypes.SERVICE_UPDATE, arr));
+			if (errorArray.length() > 0) {
+				return Response.status(Status.CONFLICT).entity(errorArray)
+						.build();
 			}
 			return Response.ok(arr).build();
-		} catch (Exception e){
+		} catch (Exception e) {
 			JSONObject jErr = new JSONObject();
 			jErr.put("error", e.getCause());
-			throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
-		} 
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+		}
 	}
-	
+
 	/**
 	 * Deleting the service description
 	 * 
 	 * @param infos
 	 *            contains a ..?SERVICE_ENDPOINT_URL=http://serviceurl
-	 * @throws JSONException 
+	 * @throws JSONException
 	 * */
 	@DELETE
-	public Response deleteService(@Context UriInfo infos) throws WebApplicationException, JSONException{
+	public Response deleteService(@Context UriInfo infos)
+			throws WebApplicationException, JSONException {
 		String serviceurl = null;
 		try {
 			Client c = (Client) req.getAttribute("client");
@@ -341,23 +382,34 @@ public class ServiceAdminResource {
 			logger.debug("deleting service by url: " + serviceurl
 					+ ", Owned by: " + owner);
 			if (c.getRole().getName().equalsIgnoreCase("admin")) {
-				//let the admin delete everything
+				// let the admin delete everything
 				serviceAdmin.removeService(serviceurl);
-			} else if ((owner != null) && (serviceAdmin.checkOwner(owner, serviceurl))) {
-				
-					serviceAdmin.removeService(serviceurl);
-				
+			} else if ((owner != null)
+					&& (serviceAdmin.checkOwner(owner, serviceurl))) {
+
+				serviceAdmin.removeService(serviceurl);
+
 			} else {
-				return Response.status(Status.UNAUTHORIZED).entity("Access denied for DN - "+owner+" to update service with the URL - "+serviceurl).build();
+				return Response
+						.status(Status.UNAUTHORIZED)
+						.entity("Access denied for DN - " + owner
+								+ " to update service with the URL - "
+								+ serviceurl).build();
 			}
 
-		} catch(IllegalArgumentException e){
-			return Response.status(Status.BAD_REQUEST).entity("Missing/Invalid query parameter: The delete request must contain a query parameter: /serviceadmin?"+ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL.getAttributeName()+" = <SERVICE URL>").build();
-		} catch (Exception e){
+		} catch (IllegalArgumentException e) {
+			return Response
+					.status(Status.BAD_REQUEST)
+					.entity("Missing/Invalid query parameter: The delete request must contain a query parameter: /serviceadmin?"
+							+ ServiceBasicAttributeNames.SERVICE_ENDPOINT_URL
+									.getAttributeName() + " = <SERVICE URL>")
+					.build();
+		} catch (Exception e) {
 			JSONObject jErr = new JSONObject();
 			jErr.put("error", e.getCause());
-			throw new WebApplicationException(Response.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
-		} 
+			throw new WebApplicationException(Response
+					.status(Status.INTERNAL_SERVER_ERROR).entity(jErr).build());
+		}
 		return Response.ok().build();
 	}
 }
