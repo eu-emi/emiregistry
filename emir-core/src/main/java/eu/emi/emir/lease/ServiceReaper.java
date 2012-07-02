c8b492f0c23d9df95e401090bcc61658b7c06419
Shiraz Memon <ahmed.shiraz.memon@gmail.com>
2012-07-02 11:06:29 +0200

    fix for https://github.com/eu-emi/emiregistry/issues/54

--- emir-core/src/main/java/eu/emi/emir/lease/ServiceReaper.java
+++ emir-core/src/main/java/eu/emi/emir/lease/ServiceReaper.java
@@ -18,15 +18,6 @@ public class ServiceReaper implements Runnable {
 	private static Logger logger = Log.getLogger(Log.EMIR_CORE, ServiceReaper.class);
 	private ServiceAdminManager sm;
 
-	/**
-	 * 
-	 */
-	public ServiceReaper() {
-
-		sm = new ServiceAdminManager();
-
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -36,15 +27,13 @@ public class ServiceReaper implements Runnable {
 	public void run() {
 		try {
 			if (logger.isTraceEnabled()) {
-				logger.trace("purging service entry");
+				logger.trace("Purging expired service records");
 			}
 			
 			if (sm==null) {
 				sm = new ServiceAdminManager();
 			}
 			sm.removeExpiredEntries();
-			
-			logger.info("Servive reaper is running");
 		} catch (Exception e) {
 			logger.warn(e.getCause());
 		}
