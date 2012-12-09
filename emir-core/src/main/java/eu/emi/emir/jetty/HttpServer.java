4db5e5508fb10d596c31609c1382c037c2bd3f32
Shiraz Memon <ahmed.shiraz.memon@gmail.com>
2012-12-09 17:43:55 +0100

    initiating filters properly in "http" mode

--- emir-core/src/main/java/eu/emi/emir/jetty/HttpServer.java
+++ emir-core/src/main/java/eu/emi/emir/jetty/HttpServer.java
@@ -17,68 +17,83 @@ import eu.emi.emir.EMIRAnonymousApplication;
 import eu.emi.emir.ServerProperties;
 import eu.emi.emir.client.util.Log;
 
-
 /**
  * Runs annonymous query interface (on http port)
  * 
  * @author a.memon
- *
+ * 
  */
 public class HttpServer {
 	@SuppressWarnings("unused")
-	private static Logger logger = Log.getLogger(Log.EMIR_HTTPSERVER, HttpServer.class);
+	private static Logger logger = Log.getLogger(Log.EMIR_HTTPSERVER,
+			HttpServer.class);
 
 	ServerProperties serverProps = null;
-    
+
 	JettyServer server = null;
-	
+
 	/**
-	 * @throws Exception 
+	 * @throws Exception
 	 * 
 	 */
 	public HttpServer(Properties props) throws Exception {
-        serverProps = new ServerProperties(props, false);
-		
+		serverProps = new ServerProperties(props, false);
+
 		EMIRJettyProperties jettyProperties = new EMIRJettyProperties(props);
-		
-		String httpsServerAddress = serverProps.getValue(ServerProperties.PROP_ADDRESS);
-		
+
+		String httpsServerAddress = serverProps
+				.getValue(ServerProperties.PROP_ADDRESS);
+
 		URL httpsServerUrl = new URL(httpsServerAddress);
-		
-		String port = serverProps.getValue(ServerProperties.PROP_ANONYMOUS_PORT);
-		
-		String address = "http://"+httpsServerUrl.getHost()+":"+port;
-		
-		URL[] url = {new URL(address)};
-		
-		server = new JettyServer(url, null, jettyProperties, getJerseyInitParams());		
-		
+
+		String port = serverProps
+				.getValue(ServerProperties.PROP_ANONYMOUS_PORT);
+
+		String address = "http://" + httpsServerUrl.getHost() + ":" + port;
+
+		URL[] url = { new URL(address) };
+
+		server = new JettyServer(url, null, jettyProperties,
+				getJerseyInitParams());
+
 	}
-	
-	public void start() throws Exception{
+
+	public void start() throws Exception {
 		server.start();
-		
+
 	}
-	
-	public void stop() throws Exception{
+
+	public void stop() throws Exception {
 		server.stop();
-		
+
 	}
-	
+
 	private Map<String, String> getJerseyInitParams() {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("javax.ws.rs.Application",
 				EMIRAnonymousApplication.class.getCanonicalName());
-		map.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, getRequestFilters());
-		map.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, getResponseFilters());
+		map.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS,
+				getRequestFilters());
+		map.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS,
+				getResponseFilters());
 		return map;
 	}
-	
+
 	/**
 	 * 
 	 */
 	private String getResponseFilters() {
-		return GZIPContentEncodingFilter.class.getName();
+		StringBuilder sb = new StringBuilder();
+
+		String s = serverProps
+				.getValue(ServerProperties.PROP_RESPONSE_INTERCEPTORS);
+
+		sb.append(GZIPContentEncodingFilter.class.getName());
+		if (s != null) {
+			sb.append(";").append(s);
+		}
+
+		return sb.toString();
 
 	}
 
@@ -86,7 +101,17 @@ public class HttpServer {
 	 * 
 	 */
 	private String getRequestFilters() {
-		return GZIPContentEncodingFilter.class.getName();
+		StringBuilder sb = new StringBuilder();
+
+		String s = serverProps
+				.getValue(ServerProperties.PROP_REQUEST_INTERCEPTORS);
+
+		sb.append(GZIPContentEncodingFilter.class.getName());
+		if (s != null) {
+			sb.append(";").append(s);
+		}
+
+		return sb.toString();
 
 	}
 }
